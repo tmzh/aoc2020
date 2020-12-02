64455c280446d4ee8944e284b47c08fd202ea1b2
Tamizh <thamizh85@@users.noreply.github.com>
2020-12-02 20:24:24 +0800

    day 1 solution

--- day1/main.go
+++ day1/main.go
@@ -7,13 +7,28 @@ import (
 	"strings"
 )
 
-func complement(a []int) (c int) {
+func product1(a []int) (c int) {
+	m := make(map[int]bool)
+
+	for _, item := range a {
+		m[2020-item] = true
+		if m[item] {
+			fmt.Println("The two entries are:", item, 2020-item)
+			c = item * (2020 - item)
+			return
+		}
+	}
+	return
+}
+
+func product2(a []int) (c int) {
 	m := make(map[[2]int]bool)
 
 	for _, item := range a {
 		for _, item1 := range a {
 			m[[2]int{item, 2020 - item - item1}] = true
 			if m[[2]int{item, item1}] {
+				fmt.Println("The three entries are:", item, item1, 2020-item-item1)
 				c = item * item1 * (2020 - item - item1)
 				return
 			}
@@ -51,6 +66,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	complement := complement(nums)
-	fmt.Println(complement)
+	fmt.Println("The two entry product is ", product1(nums))
+	fmt.Println("The three entry product is ", product2(nums))
 }
